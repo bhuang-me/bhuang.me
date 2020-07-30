891789bcdd29ef976927cb345d28d72a4dfbc58f
Brandon Huang <brandonbgda@hotmail.com>
2020-07-30 18:11:06 -0400

    adjust spacing on contact info

--- src/components/contactinfo.js
+++ src/components/contactinfo.js
@@ -2,11 +2,25 @@ import React from "react"
 import contactInfoStyles from "./contactinfo.module.css"
 
 export default function ContactInfo() {
+  function linkedInRedir() {}
+
   return (
     <div className={contactInfoStyles.containerBlack}>
       <div className={contactInfoStyles.text}>CONTACT ME</div>
-      <div className={contactInfoStyles.email}>placeholder@email.com</div>
-      <div className={contactInfoStyles.social}>linkedIn</div>
+      <div className={contactInfoStyles.email}>
+        brandon<span>bgda</span>@<span>hot</span>mail.<span>com</span>
+      </div>
+      <div className={contactInfoStyles.social}>
+        <img
+          height="22"
+          width="22"
+          src={require("./images/linkedin_logo.png")}
+          alt="LinkedInLogo"
+          onClick={() =>
+            (window.location.href = "https://linkedin.com/in/b-huang")
+          }
+        />
+      </div>
     </div>
   )
 }
